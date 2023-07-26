47b84fec75c6ce07447ab1b078febeaa27463dbb
njugudev <njugudev@pop-os.localdomain>
2023-07-26 14:06:46 +0300

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to our MOVIE App!</h2>
+      <p>
+        This is a simple app built with React and React Router. Browse the list
+        of movies and click on a movie to view its details
+      </p>
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+import Home from "./components/Home";
 
 ReactDOM.render(
   <Router>
+    <Route exact path="/" component={Home} />
     <App />
   </Router>,
   document.getElementById("root")
